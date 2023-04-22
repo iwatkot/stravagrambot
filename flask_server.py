1a30c09e5d7d2729105f55f8bc793e0fafc7f92f
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-04-22 22:04:40 +0300

    Disabled wenhook logging, updated aiogram version.

--- flask_server.py
+++ flask_server.py
@@ -11,81 +11,86 @@ from log_handler import Logger, LogTemplates
 from token_handler import Token
 from templates_handler import Constants
 
-FLASK_TEMPLATES = get_template('flask_templates')
-logger = Logger('flask_server')
-flask_log = logging.getLogger('werkzeug')
+FLASK_TEMPLATES = get_template("flask_templates")
+logger = Logger("flask_server")
+flask_log = logging.getLogger("werkzeug")
 flask_log.disabled = True
 
 app = Flask(__name__)
 bootstrap = Bootstrap4(app)
 
 
-@app.route('/webhooks/', methods=["GET"])
+@app.route("/webhooks/", methods=["GET"])
 def webhook_challenge():
-    logger.debug(LogTemplates[__name__].GET_REQUEST.format(request.full_path))
-    verify_token = request.args.get('hub.verify_token')
-    if verify_token == config('VERIFY_TOKEN'):
-        hub_challenge = request.args.get('hub.challenge')
-        logger.debug(LogTemplates[__name__].RETURNING_HUB_CHALLENGE.format(
-            hub_challenge))
-        return json.dumps({'hub.challenge': hub_challenge}), 200
+    # logger.debug(LogTemplates[__name__].GET_REQUEST.format(request.full_path))
+    verify_token = request.args.get("hub.verify_token")
+    if verify_token == config("VERIFY_TOKEN"):
+        hub_challenge = request.args.get("hub.challenge")
+        # logger.debug(LogTemplates[__name__].RETURNING_HUB_CHALLENGE.format(
+        #    hub_challenge))
+        return json.dumps({"hub.challenge": hub_challenge}), 200
 
 
-@app.route('/webhooks/', methods=["POST"])
+@app.route("/webhooks/", methods=["POST"])
 def webhook_catcher():
-    if request.content_type == 'application/json':
-        json_data = request.json
-        logger.info(LogTemplates[__name__].WEBHOOK_RECIEVED.format(json_data))
-    return '', 200
+    if request.content_type == "application/json":
+        pass
+        # json_data = request.json
+        # logger.info(LogTemplates[__name__].WEBHOOK_RECIEVED.format(json_data))
+    return "", 200
 
 
-@app.route('/stravagramoauth')
+@app.route("/stravagramoauth")
 def oauth():
     lang = locale_check(request)
-    context = FLASK_TEMPLATES['locale'][lang]
+    context = FLASK_TEMPLATES["locale"][lang]
     logger.debug(LogTemplates[__name__].GET_REQUEST.format(request.full_path))
-    telegram_id = request.args.get('telegram_id')
-    code = request.args.get('code')
-    scope = request.args.get('scope')
-    if 'activity:read_all' not in scope:
+    telegram_id = request.args.get("telegram_id")
+    code = request.args.get("code")
+    scope = request.args.get("scope")
+    if "activity:read_all" not in scope:
         logger.debug(LogTemplates[__name__].BAD_REQUEST.format(telegram_id))
-        message = FLASK_TEMPLATES['locale'][lang]['oauth_bad_message']
-        result = FLASK_TEMPLATES['locale'][lang]['oauth_bad']
-        return render_template('pages/oauth.html', context=context,
-                               result=result, message=message)
+        message = FLASK_TEMPLATES["locale"][lang]["oauth_bad_message"]
+        result = FLASK_TEMPLATES["locale"][lang]["oauth_bad"]
+        return render_template(
+            "pages/oauth.html", context=context, result=result, message=message
+        )
     else:
-        logger.debug(LogTemplates[__name__].GOOD_REQUEST.format(
-                    telegram_id, code))
+        logger.debug(LogTemplates[__name__].GOOD_REQUEST.format(telegram_id, code))
         oauth_init(telegram_id, code)
-        message = FLASK_TEMPLATES['locale'][lang]['oauth_good_message']
-        result = FLASK_TEMPLATES['locale'][lang]['oauth_good']
-        return render_template('pages/oauth.html', context=context,
-                               result=result, message=message), {
-                               "Refresh": "5; url=/"}
+        message = FLASK_TEMPLATES["locale"][lang]["oauth_good_message"]
+        result = FLASK_TEMPLATES["locale"][lang]["oauth_good"]
+        return render_template(
+            "pages/oauth.html", context=context, result=result, message=message
+        ), {"Refresh": "5; url=/"}
 
 
-@app.route('/')
+@app.route("/")
 def index_page():
     lang = locale_check(request)
-    context = FLASK_TEMPLATES['locale'][lang]
-    return render_template('pages/index.html', context=context)
+    context = FLASK_TEMPLATES["locale"][lang]
+    return render_template("pages/index.html", context=context)
 
 
-@app.route('/about')
-@app.route('/changelog')
+@app.route("/about")
+@app.route("/changelog")
 def pages():
     lang = locale_check(request)
-    context = FLASK_TEMPLATES['locale'][lang]
+    context = FLASK_TEMPLATES["locale"][lang]
     content = get_content(request.path, lang)
     return render_template(
-        'pages{}.html'.format(request.path),
-        context=context, content=content)
+        "pages{}.html".format(request.path), context=context, content=content
+    )
 
 
 def locale_check(request) -> str:
     """Returns the language code of GET request."""
-    lang = 'ru' if request.accept_languages.best_match(
-        Constants.SUPPORTED_LANGUAGES.value) == 'ru' else 'en'
+    lang = (
+        "ru"
+        if request.accept_languages.best_match(Constants.SUPPORTED_LANGUAGES.value)
+        == "ru"
+        else "en"
+    )
     return lang
 
 
@@ -105,9 +110,9 @@ def oauth_init(telegram_id: int, code: str) -> None:
 def run_server():
     port = 80
     logger.info(LogTemplates[__name__].SERVER_STARTED.format(port))
-    app.run(port=port, host='0.0.0.0')
+    app.run(port=port, host="0.0.0.0")
     logger.warning(LogTemplates[__name__].SERVER_STOPPED)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_server()
